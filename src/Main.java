fecea4651290b975fb7e9bef27ec38b243adc5df
YanaVasileva <yana.altman2002@gmail.com>
2026-05-17 23:12:47 +0300

    First commit: bonus miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 18919;
+        int rubles = 20;
+        int bonusMiles = ticketPrice / rubles;
+        System.out.println("Бонусные милли:" + bonusMiles);
+    }
+}
